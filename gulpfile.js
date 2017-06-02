e4b8b088d5860cb659032a33da795faa3fd10e74
Su Zhang <me@zhang.su>
2017-06-02 15:13:27 -0400

    Scaffold amp-google-doc

--- gulpfile.js
+++ gulpfile.js
@@ -87,6 +87,7 @@ declareExtension('amp-fx-flying-carpet', '0.1', true);
 declareExtension('amp-fx-parallax', '0.1', false);
 declareExtension('amp-gfycat', '0.1', false);
 declareExtension('amp-gist', '0.1', false);
+declareExtension('amp-google-doc', '0.1', false);
 declareExtension('amp-hulu', '0.1', false);
 declareExtension('amp-iframe', '0.1', false, 'NO_TYPE_CHECK');
 declareExtension('amp-ima-video', '0.1', false);
